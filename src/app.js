6aaca2cf0ec972697c324d4051fa3fc5727f5a4d
sundara0rajan <sundara_rajan@yahoo.com>
2021-04-30 19:42:39 +0200

    updates for pushing to heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require ('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const port = process.env.PORT || 3000
 
 
 const app = express()
@@ -115,6 +116,6 @@ app.get ('*',(req, res)=> {
 
 })
 
-app.listen (3000, ()=> {
-    console.log('Server is up at port 3000')
+app.listen (port, ()=> {
+    console.log('Server is up at port '+ port)
 })
